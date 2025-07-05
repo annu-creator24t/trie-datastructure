99315df08764b4c3c2012cb25041ee36baa055cb
annu tiwari <annu04t@gmail.com>
2025-07-05 23:42:20 +0530

    Add files via upload

---
+++ Trie.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+class TrieNode {
+    HashMap<Character, TrieNode> children;
+    boolean isEndOfWord;
+
+    public TrieNode() {
+        children = new HashMap<>();
+        isEndOfWord = false;
+    }
+}
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode(); // Initialize root node
+    }
+}
